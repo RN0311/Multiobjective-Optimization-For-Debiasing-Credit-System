cdcdad6c46d06134b1253061ca0befaf7b2efbb4
Rashmi Nagpal <rashminagpal.nagpal@gmail.com>
2023-09-25 21:53:18 +0300

    Updated multi-objective-optimization-ngsa2-german-credit.py

--- multiobjective-optimizers/multi-objective-optimization-ngsa2-german-credit.py
+++ multiobjective-optimizers/multi-objective-optimization-ngsa2-german-credit.py
@@ -33,11 +33,9 @@ def calculate_equalized_odds(xgboost_y_pred, X_test, privileged_group, unprivile
     privileged_true_labels = y_test[(X_test['age'] > 18) & (X_test['statussex_A91'].isin(privileged_group) | X_test['statussex_A93'].isin(privileged_group) | X_test['statussex_A94'].isin(privileged_group))]
     unprivileged_true_labels = y_test[(X_test['age'] > 18) & (X_test['statussex_A92'].isin(unprivileged_group))]
 
-    # Calculated True Positive Rate (TPR) and False Positive Rate (FPR) for both groups
     privileged_confusion_matrix = confusion_matrix(privileged_true_labels, privileged_labels)
     unprivileged_confusion_matrix = confusion_matrix(unprivileged_true_labels, unprivileged_labels)
 
-    # Calculated True Positive Rate (TPR) and False Positive Rate (FPR) for both groups
     if privileged_confusion_matrix.size > 0:
         privileged_tpr = privileged_confusion_matrix[1, 1] / (privileged_confusion_matrix[1, 0] + privileged_confusion_matrix[1, 1])
         privileged_fpr = privileged_confusion_matrix[0, 1] / (privileged_confusion_matrix[0, 0] + privileged_confusion_matrix[0, 1])
@@ -66,7 +64,6 @@ sensitive_features = ['statussex_A91', 'statussex_A92', 'statussex_A93', 'status
 privileged_group = ['statussex_A91', 'statussex_A93', 'statussex_A94']  # Privileged group: all men above the age of 18
 unprivileged_group = ['statussex_A92']  # Unprivileged group: all women above the age of 18
 
-# Define the problem as a multi-objective optimization problem
 class CreditRiskProblem(Problem):
     def __init__(self, X_train, y_train, X_test, y_test):
         super().__init__(n_var=X_train.shape[1],
@@ -87,10 +84,8 @@ class CreditRiskProblem(Problem):
             'seed': [42]
         }
 
-        # Created the XGBoost classifier
         xgb_classifier = xgb.XGBClassifier()
 
-        # Performed grid search with cross-validation
         grid_search = GridSearchCV(estimator=xgb_classifier, param_grid=param_grid, cv=3, scoring='accuracy')
         grid_search.fit(X_train, y_train)
 
@@ -98,7 +93,6 @@ class CreditRiskProblem(Problem):
         best_params = grid_search.best_params_
         print("Best Hyperparameters:", best_params)
 
-        # Train the final model with the best hyperparameters
         self.model = xgb.XGBClassifier( **best_params)
 
 
@@ -113,7 +107,6 @@ class CreditRiskProblem(Problem):
         group_fairness = calculate_equalized_odds(y_pred, self.X_test, privileged_group, unprivileged_group)
         print("fairness\n", individual_fairness)
 
-        # Modify the fairness metric to convert it into a minimization problem
         fairness_metric = -individual_fairness
 
         # Created a trade-off between accuracy and fairness using a weighted sum
@@ -123,12 +116,9 @@ class CreditRiskProblem(Problem):
         # Combine accuracy and fairness using the weighted sum
         combined_metric = weight_accuracy * acc + weight_fairness * fairness_metric
         
-        # Combine accuracy and fairness using the weighted sum
-        combined_metric = weight_accuracy * acc + weight_fairness * fairness_metric
         
         out["F"] = [-combined_metric]  # Minimize the combined metric
 
-# Load the data
 data = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/statlog/german/german.data',
                    header=None, sep=' ')
 
@@ -157,7 +147,6 @@ X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=
 smote_tomek = SMOTETomek(random_state=42)
 X_train_balanced, y_train_balanced = smote_tomek.fit_resample(X_train, y_train)
 
-# Defined the Pareto optimization problem and algorithm
 problem = CreditRiskProblem(X_train_balanced, y_train_balanced, X_test, y_test)
 algorithm = NSGA2(pop_size=1000,
                   n_offsprings=50,
